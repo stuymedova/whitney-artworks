1d7c6bbd3d6b15003c0476f406db78068422023c
Sofya Tuymedova <stuymedova@gmail.com>
2021-06-07 11:03:41 +0300

    feat: pagination and infinite scroll

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <ais-instant-search :search-client='searchClient' index-name='artworks'>
-    <ais-search-box placeholder='Search'/>
-    <ais-hits>
+    <ais-search-box placeholder='Search' />
+    <ais-configure :hitsPerPage='100' />
+
+    <app-infinite-hits>
       <div slot='item' slot-scope='{ item }'>
         <p>{{ item.title }}</p>
       </div>
-    </ais-hits>
+    </app-infinite-hits>
+
   </ais-instant-search>
 </template>
 
 <script>
   import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+  import AppInfiniteHits from './components/InfiniteHits';
 
   export default {
+    components: { AppInfiniteHits },
     data() {
       return {
         searchClient: instantMeiliSearch(
           process.env.VUE_APP_MEILISEARCH_HOST,
-          process.env.VUE_APP_MEILISEARCH_API_KEY
+          process.env.VUE_APP_MEILISEARCH_API_KEY,
+          {
+            paginationTotalHits: 1000,
+            primaryKey: 'id'
+          }
         ),
         show: true,
         isExpanded: true
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import InstantSearch from 'vue-instantsearch';
+import VueObserveVisibility from 'vue-observe-visibility';
 
 Vue.use(InstantSearch);
+Vue.use(VueObserveVisibility);
 Vue.config.productionTip = false;
 
 new Vue({
